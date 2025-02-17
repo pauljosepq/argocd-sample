9592948d04141715a46bfb9f6e7131d1def1c059
Paul Paniagua <pauljosepq2@gmail.com>
2025-02-16 23:39:03 -0400

    Improve vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
+    allowedHosts: true,
+    port: 4000,
+    cors: true,
     proxy: {
       '/api': {
         target: 'http://backend:3000',
